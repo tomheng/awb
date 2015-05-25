9481b705206e223dab889121fd6985457bd2b60e
tomheng <zhm20070928@gmail.com>
2015-05-25 19:03:30 +0800

    improve code

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httputil"
 	"os"
+	"os/signal"
 	"runtime"
 	"sync"
 	"time"
@@ -20,13 +22,13 @@ var (
 )
 
 const (
-	CN = "Another Web Bench"
-	SN = "awb"
+	CN      = "Another Web Bench"
+	SN      = "awb"
 	VERSION = "0.1"
 )
 
 type jober interface {
-	perform() *jobResult
+	perform() jobResult
 }
 
 type bench struct {
@@ -35,6 +37,8 @@ type bench struct {
 	Timelimit   int
 	Jobs        chan jober
 	Br          *benchResult
+	stoped      bool
+	sync.Mutex
 }
 
 type benchResult struct {
@@ -50,13 +54,14 @@ func newBench(r, c, t int) *bench {
 		Requests:    r,
 		Concurrency: c,
 		Timelimit:   t,
-		Jobs:        make(chan jober),
+		Jobs:        make(chan jober, c * 2),
 		Br:          &benchResult{},
 	}
 }
 
 func (b *bench) start(jobs ...jober) {
 	var wg sync.WaitGroup
+	//set timer if with timelimit
 	go func() {
 		if b.Timelimit <= 0 {
 			return
@@ -66,6 +71,7 @@ func (b *bench) start(jobs ...jober) {
 	}()
 	fmt.Printf("This is %s(%s), Version %s \n\n", SN, CN, VERSION)
 	fmt.Println("start Benchmarking ...(be patient)")
+	start := time.Now()
 	for _, job := range jobs {
 		go b.produce(job)
 	}
@@ -75,6 +81,7 @@ func (b *bench) start(jobs ...jober) {
 		go b.consume(&wg)
 	}
 	wg.Wait()
+	b.Br.Spend_time = time.Since(start)
 	b.printResult()
 }
 
@@ -98,68 +105,94 @@ Transfer rate:          %.2f [Kbytes/sec] received
 		b.Br.Failed_count,
 		b.Br.Total_transferred,
 		b.Br.Html_transferred,
-		float64(complete_request) / b.Br.Spend_time.Seconds(),
-		float64(b.Br.Total_transferred) / 1024 / b.Br.Spend_time.Seconds(),
+		float64(complete_request)/b.Br.Spend_time.Seconds(),
+		float64(b.Br.Total_transferred)/1024/b.Br.Spend_time.Seconds(),
 	)
 }
 
 func (b *bench) stop() {
+	b.Lock()
+	defer b.Unlock()
+	if b.stoped {
+		return
+	}
+	b.stoped = true
 	close(b.Jobs)
+}
+
+func (b *bench) processResult(result jobResult) {
+	b.Lock()
+	defer b.Unlock()
+	if b.stoped {
+		return
+	}
+	if result.success {
+		b.Br.Success_count += 1
+	} else {
+		b.Br.Failed_count += 1
+	}
+	b.Br.Html_transferred += result.content_length
+	b.Br.Total_transferred += result.total_length
 }
 
 func (b *bench) produce(job jober) {
 	i := 0
 	for {
+		b.Lock()
+		if b.stoped {
+			b.Unlock()
+			break
+		}
 		if i >= b.Requests {
+			b.Unlock()
 			b.stop()
 			break
 		}
 		i += 1
 		b.Jobs <- job
+		b.Unlock()
 	}
 }
 
+//concurrency unit
 func (b *bench) consume(wg *sync.WaitGroup) {
 	for job := range b.Jobs {
-		result := job.perform()
-		if result.success {
-			b.Br.Success_count += 1
-		} else {
-			b.Br.Failed_count += 1
-		}
-		b.Br.Html_transferred += result.content_length
-		b.Br.Total_transferred += result.total_length
-		b.Br.Spend_time += result.spend_time
+		//Asynchronous process job result
+		go b.processResult(job.perform())
 	}
 	wg.Done()
 }
 
 type httpJob struct {
-	client  *http.Client
-	request *http.Request
+	timeout int64
+	method  string
+	url     string
 }
 
-func newHttpJob(timeout int64, method string, url string) *httpJob {
-	hr, err := http.NewRequest(method, url, nil)
-	if err != nil {
-		panic(err)
-	}
-	return &httpJob{
-		&http.Client{
-			Timeout: time.Millisecond * time.Duration(timeout),
-		},
-		hr,
+func newHttpJob(timeout int64, method string, url string) httpJob {
+	return httpJob{
+		timeout,
+		method,
+		url,
 	}
 }
 
-func (hj *httpJob) perform() *jobResult {
+func (hj httpJob) perform() jobResult {
 	var content_length, total_length int64
 	var content_type string
+	client := http.Client{
+		Timeout: time.Millisecond * time.Duration(hj.timeout),
+	}
+	hr, err := http.NewRequest(hj.method, hj.url, nil)
+	if err != nil {
+		log.Fatal("failed to initialize http request, please try again")
+	}
+	hr.Header.Set("User-Agent", SN+"/"+VERSION+" ("+CN+")")
 	start := time.Now()
-	response, _ := hj.client.Do(hj.request)
+	response, err := client.Do(hr)
 	spend_time := time.Since(start)
 	success := false
-	if response.StatusCode == 200 {
+	if err == nil && response.StatusCode == 200 {
 		success = true
 		if response.ContentLength == -1 {
 			body, _ := ioutil.ReadAll(response.Body)
@@ -170,7 +203,7 @@ func (hj *httpJob) perform() *jobResult {
 		content_length = response.ContentLength
 		content_type = response.Header.Get("Content-Type")
 	}
-	return &jobResult{
+	return jobResult{
 		success,
 		spend_time,
 		content_type,
@@ -197,6 +230,14 @@ func main() {
 	URL := flag.Arg(0)
 	b := newBench(*requests, *concurrency, *timelimit)
 	job := newHttpJob(0, "GET", URL)
+	//listen signal
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt, os.Kill)
+		// Block until a signal is received.
+		<-c
+		b.stop()
+	}()
 	b.start(job)
 }
 
